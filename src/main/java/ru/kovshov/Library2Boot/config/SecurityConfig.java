a81595070e7243501232b01e9e9976efc0a9ff28
Alexandr <Alexandr>
2022-11-02 18:13:10 +0300

    security

--- src/main/java/ru/kovshov/Library2Boot/config/SecurityConfig.java
+++ src/main/java/ru/kovshov/Library2Boot/config/SecurityConfig.java
@@ -1,23 +1,61 @@
 package ru.kovshov.Library2Boot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.kovshov.Library2Boot.security.AuthProvaiderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import ru.kovshov.Library2Boot.service.UserDetalSevice;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProvaiderImpl authProvaider;
+
+    private final UserDetalSevice userDetalSevice;
 
     @Autowired
-    public SecurityConfig(AuthProvaiderImpl authProvaider) {
-        this.authProvaider = authProvaider;
+    public SecurityConfig(UserDetalSevice userDetalSevice) {
+        this.userDetalSevice = userDetalSevice;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvaider);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/library")
+                .failureUrl("auth/login?error");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetalSevice);
 
     }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+//    @Bean
+//    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authz) -> authz
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
 
 }
+
+
